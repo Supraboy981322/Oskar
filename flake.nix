1dabb374136d0c99cf2abca9148a2a92adf86b5e
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-07-13 18:52:04 -0500

    a nix flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "better chibicc";
+
+  inputs = {
+    # nixpkgs unstable for latest versions
+    pkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs: 
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        repo_root = builtins.toString ./.;
+
+        pkgs = import nixpkgs { inherit system; };
+
+        deps = with pkgs; [
+          gcc
+          gdb
+          glibc
+          glibc.static
+          stdenv
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = deps;
+          nativeBuildInputs = deps;
+          env = let
+            concat = paths: (builtins.concatStringsSep ":" paths) + ":";
+            cc = pkgs.stdenv.cc;
+          in {
+            LIBRARY_PATH = concat [
+              "${pkgs.glibc.out}/lib"
+              "${pkgs.gcc.out}/lib"
+            ];
+            LD_LIBRARY_PATH = concat [
+              "${cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${cc.version}"
+            ];
+          };
+        };
+      })
+    );
+}
